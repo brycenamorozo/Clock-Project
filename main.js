d92979655ccfd3387ca8e5390e2c3247d5781ef0
richard <brycenamor808@gmail.com>
2020-01-13 09:36:03 -1000

    third commit

--- main.js
+++ main.js
@@ -1,25 +1,15 @@
-var newDate = newDate();
-newDate.setDate(newDate.getDate());
-setInterval( function t1() {
-
-var getSeconds = newDate().getSeconds();
-
-function ("#seconds").html(( getseconds < 10 ? "0" : "" ) + getseconds); },1000);
-
-//creates date and seconds and adds zeros to the number
-
-setInterval( function t2(){
-
- var getMinutes = newDate().getMinutes();
-
- function("#minutes").html((getminutes < 10 ? "0" : "" ) + getminutes); },1000);
-
-// same process but for minutes
-
-setInterval( function t3(){
-
- var getHours = newDate().getHours();
-
- function("#hours").html((gethours < 10 ? "0" : "" ) + gethours); },1000);
-
-//same process but for hours
+function startTime() {
+  var time = new Date();
+  var h = time.getHours();
+  var m = time.getMinutes();
+  var s = time.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  document.getElementById('time').innerHTML =
+  h + ":" + m + ":" + s;
+  var t = setTimeout(startTime, 500);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};
+  return i;
+}
