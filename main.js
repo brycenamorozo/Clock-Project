66e31a0c65ce97b36eb8729434048fcb3243b761
richard <brycenamor808@gmail.com>
2020-01-15 10:36:55 -1000

    fourth commit

--- main.js
+++ main.js
@@ -1,14 +1,34 @@
 function startTime() {
+
+  var ampm = "pm"
+
   var time = new Date();
+  
   var h = time.getHours();
+ 
   var m = time.getMinutes();
+  
   var s = time.getSeconds();
+ 
   m = checkTime(m);
   s = checkTime(s);
+
+  if (h < 12) {
+    ampm = "AM"
+  }
+  if (h > 12) {
+    h -=12
+    ampm = "PM"
+  }
+  if (h < 10) {
+    h = "0" + h
+  }
+
   document.getElementById('time').innerHTML =
-  h + ":" + m + ":" + s;
+  h + ":" + m + ":" + s + " " + ampm;
   var t = setTimeout(startTime, 500);
 }
+
 function checkTime(i) {
   if (i < 10) {i = "0" + i};
   return i;
