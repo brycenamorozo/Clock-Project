2e5fb5de746763147ef248982859ee7c32a30cfc
richard <brycenamor808@gmail.com>
2020-01-15 10:57:13 -1000

    fifth commit

--- main.js
+++ main.js
@@ -9,9 +9,8 @@ function startTime() {
   var m = time.getMinutes();
   
   var s = time.getSeconds();
- 
-  m = checkTime(m);
-  s = checkTime(s);
+
+  //gets time from date set
 
   if (h < 12) {
     ampm = "AM"
@@ -24,12 +23,23 @@ function startTime() {
     h = "0" + h
   }
 
+  /*changes clock from am to pm
+    subtracts 12 hours when greater than 12
+    and adds pm at end instead of am when 
+    greater than 12
+  */
+
+  m = checkTime(m);
+  s = checkTime(s);
+
   document.getElementById('time').innerHTML =
   h + ":" + m + ":" + s + " " + ampm;
   var t = setTimeout(startTime, 500);
 }
+  // puts together hours minutes and seconds in html
 
 function checkTime(i) {
   if (i < 10) {i = "0" + i};
   return i;
 }
+  //adds zero to time if less than 10
